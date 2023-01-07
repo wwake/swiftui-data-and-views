a159a16796ece33035f90e41184289a4a5852aa0
Bill Wake <william.wake@acm.org>
2023-01-07 16:01:59 -0600

    Initial version

---
+++ swift-ui-data-and-views/swift-ui-data-and-views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift-ui-data-and-views
+//
+//  Created by Bill Wake on 1/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-ui-data-and-views/swift-ui-data-and-views/swift_ui_data_and_viewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_data_and_viewsApp.swift
+//  swift-ui-data-and-views
+//
+//  Created by Bill Wake on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_data_and_viewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
